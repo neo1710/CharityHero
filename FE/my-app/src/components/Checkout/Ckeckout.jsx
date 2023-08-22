d09b8cb9b860c706cc6cd59554711f374595f02c
Upendra Pal <up06072000@gmail.com>
2023-08-22 20:21:40 +0530

    Project Set-up

---
+++ FE/my-app/src/components/Checkout/Ckeckout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from '<div className="" />
+<Checkout></Checkout>.module.css'
+const Ckeckout = () => {
+  return (
+    <div>
+      Checkout Page
+    </div>
+  )
+}
+
+export default Ckeckout
