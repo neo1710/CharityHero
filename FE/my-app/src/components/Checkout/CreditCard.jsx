adb940b8786fff7d9ccc02cb99b2f379f4472434
Upendra Pal <up06072000@gmail.com>
2023-08-24 00:59:11 +0530

    Day-2 task complete

---
+++ FE/my-app/src/components/Checkout/CreditCard.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import Style from './Checkout.module.css'
+
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Input,
+  Button,
+} from '@chakra-ui/react'
+const CreditCard = () => {
+
+
+  const handlepayment=()=>{
+
+  }
+  return (
+    <div>
+    <div className={Style.Upi_id_card}>
+    <FormControl isRequired>
+  <FormLabel>Enter card number</FormLabel>
+  <Input placeholder='Enter card number' type='Number' isRequired/>
+</FormControl>
+    </div>
+    <div className={Style.Upi_id_cvc}>
+      <div className={Style.account_number} style={{width:"60%"}}>
+    <FormControl isRequired>
+  <FormLabel>Card expiry date</FormLabel>
+  <Input placeholder="MM/YYYY" />
+</FormControl>
+
+      </div>
+
+      
+      <div className={Style.account_number} style={{width:"30%"}}>
+  <FormControl isRequired>
+  <FormLabel>CVC</FormLabel>
+  <Input type="Number"  placeholder='CVC' />
+</FormControl>
+      </div>
+    </div>
+    <div className={Style.Upi_id_card}>
+    <FormControl isRequired>
+  <FormLabel>Card holder name</FormLabel>
+  <Input  placeholder="Card holder name" />
+</FormControl>
+    </div>
+    <Button onClick={handlepayment} m={"1rem"}  w={"90%"} colorScheme='teal' variant='solid'>
+    Button
+  </Button>
+    </div>
+  )
+}
+
+export default CreditCard
