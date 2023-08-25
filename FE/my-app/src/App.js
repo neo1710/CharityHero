34c3800d2b91cb1817796544d8c4a070b500aca1
Upendra Pal <up06072000@gmail.com>
2023-08-25 17:06:07 +0530

    Day-4

--- FE/my-app/src/App.js
+++ FE/my-app/src/App.js
@@ -17,14 +17,10 @@ function App() {
         <NavBar/>
   {/* <Checkout /> */}
   <AllRoutes/>
-<<<<<<< HEAD
    {/* <Home/> */}
 
    <Footer/>
 {/* <Signup/> */}
-=======
-   <Footer/>
->>>>>>> 1f726495ee937c30634f14557d724dcfa80ae661
     </div>
   );
 }
--- FE/my-app/src/routes/AllRoutes.jsx
+++ FE/my-app/src/routes/AllRoutes.jsx
@@ -8,13 +8,6 @@ import {Login} from '../pages/Login';
 import Home from "../pages/Home";
 import Checkout from '../components/Checkout/Ckeckout';
 import DonationDetails from "../pages/DonationDetails";
-<<<<<<< HEAD
-import Requests from "../pages/Requests";
-import SingleRequestCard from "../components/Requests/SingleRequestCard";
-import UserPage from "../pages/UserPage";
-=======
-
->>>>>>> 1f726495ee937c30634f14557d724dcfa80ae661
 export default function AllRoutes(){
     return(
 
@@ -22,16 +15,12 @@ export default function AllRoutes(){
         <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/checkout" element={<Checkout/>} />
-        <Route path="/donait/:id" element={<DonationDetails/>} />
+        <Route path="/donate/:id" element={<DonationDetails/>} />
         <Route path="/requests" element={<Requests/>}/>
         <Route path="/requests/:id" element={<SingleRequestCard/>}/>
         <Route path="/user" element={<UserPage/>}/>
-<<<<<<< HEAD
-        {/* <Route path="/user" element={<UserPage/>}/> */}
-=======
         <Route path="/signup" element={<Signup/>}/>
         <Route path="/login" element={<Login/>}/>
->>>>>>> 1f726495ee937c30634f14557d724dcfa80ae661
     </Routes>
   </>
 
