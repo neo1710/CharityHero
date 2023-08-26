d82a5f2e8c7607f1ff57fcc972ac3e0eaf19f8d3
neo1710 <neeraj3dworld@gmail.com>
2023-08-27 01:29:15 +0530

    fw23_0626

--- FE/my-app/src/components/SigninForm.jsx
+++ FE/my-app/src/components/SigninForm.jsx
@@ -1,30 +1,72 @@
 import axios from 'axios';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
-
-
+import {useNavigate} from 'react-router-dom'
+import {
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+  Spinner
+} from '@chakra-ui/react'
 
   export const SigninForm=()=>{
   const [name,setName]=useState("");
   const [email,setEmail]=useState("");
   const [pass,setPass]=useState("");
   const [org,setOrg]=useState("");
+  let [bool,setBool]=useState(false);
+  let [load,setload]=useState(false);
+  let [err,setErr]=useState(false);
+  let navigate=useNavigate()
+  useEffect(()=>{
+if(bool){
+  setTimeout(() => {
+    navigate('/login');
+  }, 2000);
 
+}
+  },[bool])
 function signin(e){
     e.preventDefault();
 let data={email,password:pass,organization:org,name};
+setload(true);
   axios.post(`https://ivory-ox-kilt.cyclic.cloud/user/register`,data).then((res)=>{
 console.log(res);
+setBool(true);
+setload(false)
   }).catch((err)=>{
     console.log(err);
+    setload(false)
   })  
+  setName("");
+  setEmail("");
+  setOrg("");
+  setPass("");
 }
 
   console.log({name,email,pass,org});
 
-    return (
-        <DIV>
-            <label>Register Here</label>
+    return (<>
+      { load?<Spinner size='xl' />:   <DIV>
+       {bool? <Alert
+        status='success'
+        variant='subtle'
+        flexDirection='column'
+        alignItems='center'
+        justifyContent='center'
+        textAlign='center'
+        height='200px'
+      >
+        <AlertIcon boxSize='40px' mr={0} />
+        <AlertTitle color={'#02a95c'} mt={4} mb={1} fontSize='lg'>
+          User has been registered
+        </AlertTitle>
+        <AlertDescription color={'#02a95c'} maxWidth='sm'>
+          Thanks for Signing up.
+        </AlertDescription>
+      </Alert> : <div>
+            <h1>Register Here</h1><br />
 <form >
     <label>Name</label><br />
 <input value={name} onChange={(e)=>{setName(e.target.value)}}
@@ -40,25 +82,33 @@ type="text" required placeholder="name"/><br />
          type="text" required placeholder="organization"/><br />
          <button type='submit' onClick={(e)=>{signin(e)}}>SUBMIT</button>
 </form>
-
-        </DIV>
+</div>}
+  
+        </DIV>}
+        </>
     )
   }
 
 
   const DIV=styled.div`
-width: 50%;
+width: 40%;
 box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
 margin: auto;
 background-color: white;
 padding: 20px;
 input{
-   border :green 1px solid ;
+   border :#02a95c 1px solid ;
+  
+}
+h1{
+  color:#02a95c;
 }
 button{
-    background-color: green;
+    background-color: #02a95c;;
     color: white;
+    padding: 4px;
     border: 1px lightgreen solid;
+    margin-top: 10px;
 }
 button:hover{
     background-color: lightgray;
